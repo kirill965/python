18677f2b9d12353d61c6f743cfac78b66db7a168
kirill965 <volik_kirill@eclipso.eu>
2021-02-23 13:34:48 +0100

    Change hello_user.py

--- hello_user.py
+++ hello_user.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
-user_ = input("Please enter your name here: \n-> ")
+import sys
+if len(sys.argv) == 1:
+    user_ = input("Please enter your name here: \n-> ")
+else:
+    user_ = sys.argv[1]
 
 if __name__ == "__main__":
-    print(f'Hello, {user_}! How are you?')
+    print(f'Hello, {user_.title()}! How are you?')
