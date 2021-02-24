c8a73ce008a2e87703b1b4ef18041a7bf7d2d57b
kirill_v <volik_kirill@eclipso.eu>
2021-02-24 15:47:42 +0100

    adding 1.Basics

---
+++ 1.Basics/simple_math.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""This file contains operators of simple math.
+   This is multiline comment."""
+
+#This is single line comment
+
+# ** -> 2**3=8
+print(f"Power 3 of 2 is {str(2**3)}.")
+
+# 22%8=6(8*2 + 6) -> modulo
+print(f"Modulo of 22 and 8 is {str(22%8)}.")
+
+# 22//8=2 -> integer division(integer type)
+print(f"Result of integer division of 22 and 8 is {str(22//8)}.")
+
+# 22/8=2.75 -> simple division(float type)
+print(f"Result of division of 22 and 8 is {str(22/8)}.")
+
+#3*5=15 -> multiplication
+print(f"Result of multiplication of 3 and 5 is {str(5*3)}.")
+
+#5-2=3 -> substraction
+print(f"Result of substraction of 5 and 2 is {str(5-3)}.")
+
+#2+2=4 -> addition
+print(f"Result of addition 2 and 2 is {str(2+2)}.")
