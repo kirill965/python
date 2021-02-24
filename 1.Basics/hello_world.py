9bd0b941c208766880a2298cb87a10178a459b41
kirill_v <volik_kirill@eclipso.eu>
2021-02-24 15:46:16 +0100

    Making some basics

--- 1.Basics/hello_world.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-
-msg = "Hello, World!"
-
-if __name__ == "__main__":
-    print(msg)
-    print("What a wonderful day?")
--- 1.Basics/input_.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-name_ = input('Enter your name: ')
-
-if __name__ == "__main__":
-    print(f'You entered name {name_.title()}.')
