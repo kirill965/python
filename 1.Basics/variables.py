0e9f36ec02ec56500004564e26963c171f1d1f83
kirill_v <volik_kirill@eclipso.eu>
2021-02-24 16:51:10 +0100

    Creating new branch and file variables.py

---
+++ 1.Basics/variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+"""Variables in Python."""
+#Variables are like a box for values in python
+#example_1
+msg = "Hello, World!" #msg -> variable name, "Hello, World!" -> value, = -> linking between variable name and value(It's called assignment statement.)
+
+#instead of writing print("Hello, World!") we can use next statement print(msg)
+print(msg)
